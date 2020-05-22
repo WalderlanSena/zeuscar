895a35f254682a393fb233855aa24f765fb012fc
Walderlan Sena <senawalderlan@gmail.com>
2020-05-22 13:53:03 -0300

    feature: Added Default Container component.

---
+++ src/components/DefaultContainer/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Corporation = styled.div`
+  text-align: center;
+  padding: 30px;
+  p {
+    font-weight: bold;
+    color: #787878;
+    a {
+      text-decoration: none;
+      color: #ffdd55;
+    }
+  }
+`;
