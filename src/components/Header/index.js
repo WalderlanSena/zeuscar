8589a53a853be3e18e93ccfa85b0f0ac74d43671
Walderlan Sena <senawalderlan@gmail.com>
2020-05-23 13:14:25 -0300

    bugfix: Correct menu error and add side menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import { Menu, MenuItem } from "@material-ui/core";
 import Logo from "../../assets/zeuscarLogo.png";
 import { Link } from "react-router-dom";
 import history from "./../../services/history";
-import DriveEtaIcon from "@material-ui/icons/DriveEta";
+import DriveEtaOutlinedIcon from "@material-ui/icons/DriveEtaOutlined";
+import DashboardOutlinedIcon from "@material-ui/icons/DashboardOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -21,20 +28,53 @@ const useStyles = makeStyles((theme) =>
     title: {
       flexGrow: 1,
     },
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: "auto",
+    },
   })
 );
 
 export default function Header() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setOpen(false);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const list = (anchor) => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem button onClick={() => history.push("/administracao")}>
+          <ListItemIcon>
+            <DashboardOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Administração"} />
+        </ListItem>
+      </List>
+      <List>
+        <ListItem button onClick={() => history.push("/")}>
+          <ListItemIcon>
+            <DriveEtaOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Ofertas"} />
+        </ListItem>
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
@@ -48,30 +88,20 @@ export default function Header() {
               <img src={Logo} alt={"Logo"} />
             </Link>
           </div>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
+          <IconButton
+            onClick={() => setOpen(true)}
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
           >
-            <DriveEtaIcon />
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={() => history.push("/administracao")}>
-              Administração
-            </MenuItem>
-            <MenuItem onClick={() => history.push("/ofertas")}>
-              Ofertas
-            </MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <SwipeableDrawer open={open} onClose={toggleDrawer("left", false)}>
+        {list("left")}
+      </SwipeableDrawer>
     </div>
   );
 }
